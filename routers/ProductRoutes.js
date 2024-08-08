fe2122186e57e78f563d308b1c5b7ef8efd8a0e6
GIRIDHARAN R <124025064+bhuvanesh033@users.noreply.github.com>
2024-08-08 10:41:47 +0530

    removed a function

--- routers/ProductRoutes.js
+++ routers/ProductRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 const auth = require('../middleware/auth');
-router.get('/getproducts',auth,productController.getAllproducts);
+router.get('/getproducts',productController.getAllproducts);
 router.post('/addproducts', productController.addProduct);
 router.put('/updateproducts/:id', productController.updateProduct);
 router.delete('/deleteproducts/:id', productController.deleteProducts);
